d4577a87aa4142008fbe34358b6e49b6a9a9baca
hisanamuhammed <hisanamuhammed01@gmail.com>
2022-12-19 13:53:59 +0530

    img size changed

--- store/models.py
+++ store/models.py
@@ -3,7 +3,7 @@ from accounts.models import Account
 from category.models import Category 
 from django.db.models import Avg
 from django.urls import reverse
-
+from PIL import Image
 
 # Create your models here.
 
@@ -12,13 +12,22 @@ class Product(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField(max_length=500, blank=True)
     price = models.IntegerField()
-    images = models.ImageField(upload_to='photos/products')
     stock = models.IntegerField()
     is_available = models.BooleanField(default=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
+    images = models.ImageField(upload_to='photos/products')
 
+    def save(self, *args, **kwargs):
+        img = Image.open(self.images.name)
+        if img.height > 400 or img.width > 400:
+            output_size = (350, 350)
+            img.thumbnail(output_size)
+            img.save(self.images.name)
+
+    def get_url(self):
+        return reverse('product_detail', args=[self.category.slug, self.slug])
 
     def get_url(self):
         return reverse('product_detail', args=[self.category.slug, self.slug])
